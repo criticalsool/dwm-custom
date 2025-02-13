82c3ab34f30867964a136cad7b6edf86237460bf
criticalsool <164774099+criticalsool@users.noreply.github.com>
2025-02-13 18:48:25 +0100

    UPDATE config.def.h with custom values change in color name variables adding add sound controls

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,32 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_red[]         = "#300000";
-static const char *colors[][3]      = {
-	/*               fg         bg         	border   */
-	[SchemeNorm] = { col_gray3, col_gray1,	col_gray2 },
-	[SchemeSel]  = { col_gray4, col_red,	col_red  },
+static const unsigned int borderpx  	= 1;        /* border pixel of windows */
+static const unsigned int snap      	= 32;       /* snap pixel */
+static const int showbar            	= 1;        /* 0 means no bar */
+static const int topbar             	= 1;        /* 0 means bottom bar */
+static const char *fonts[]          	= { "monospace:size=16" };
+static const char dmenufont[]       	= "monospace:size=16";
+static const char normbordercolor[] 	= "#3B4252";
+static const char normbgcolor[]     	= "#300000";
+static const char normfgcolor[]     	= "#D8DEE9";
+static const char selbordercolor[]  	= "#434C5E";
+static const char selbgcolor[]      	= "#300000";
+static const char selfgcolor[]      	= "#ECEFF4";
+static const char *colors[][3]      	= {
+    /*               fg           bg           border   */
+    [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+    [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor },
 };
 
 static const char *const autostart[] = {
-	"slstatus", NULL,
-	"feh --bg-fill ~/Images/wallpaper.png"
-	"gnome-terminal", NULL,
+	"slstatus", NULL,												/* slstatus bar */
+	"sh", "-c", "feh --bg-fill ~/Images/wallpaper.png", NULL,		/* setup background */
+	"firefox", NULL,												/* start firefox */
 	NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "Terminal", "Internet", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -34,20 +35,20 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      			instance    title       tags mask     isfloating   monitor */
-	{ "Gnome-terminal",  	NULL,       NULL,       1,       		0,           -1 },
-	{ "Navigator",  			NULL,       NULL,       2,       		0,           -1 },
+	{ "Gnome-terminal",  	NULL,       NULL,       0,       		0,           -1 },
+	{ "firefox-esr",	  	NULL,       NULL,       2,       		0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact     = 0.6; 	/* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    	/* number of clients in master area */
+static const int resizehints = 1;    	/* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; 	/* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },    	/* first entry is default */
+	{ "><>",      NULL },    	/* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -64,11 +65,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
-
-static const Key keys[] = {
+const Key keys[] = {
 	/* modifier                     key        function        argument */
+    { MODKEY,                       XK_Insert, spawn,          SHCMD ("amixer sset Master 5%- unmute")},
+    { MODKEY,                       XK_Home,   spawn,          SHCMD ("amixer sset Master toggle")},
+    { MODKEY,                       XK_Prior,  spawn,          SHCMD ("amixer sset Master 5%+ unmute")},
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -80,7 +83,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,			            XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -122,4 +125,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
