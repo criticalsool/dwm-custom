7e2b7468d591b3cfae0bc17d1ea27eacbeadc314
criticalsool <164774099+criticalsool@users.noreply.github.com>
2025-02-13 19:49:08 +0100

    UPDATE termcmd (shit now you can judge...)

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 const Key keys[] = {
 	/* modifier                     key        function        argument */
     { MODKEY,                       XK_Insert, spawn,          SHCMD ("amixer sset Master toggle")},		/*mute/unmute*/
